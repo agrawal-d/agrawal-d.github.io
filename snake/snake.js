b2b42decbfb4d9176c4406bc23ca475eb520cc9c
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-04-20 01:53:32 +0530

    Add levels

--- snake/snake.js
+++ snake/snake.js
@@ -2,7 +2,12 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var grid = 200;
 var interval;
+var levelUp = 5000;
+var powerUpColor = 'rgb(15, 0, 255)';
+var powerProb = 0.7;
 var scale = 40;
+var wallColor = 'white';
+var walls = [];
 var w = window.innerWidth;
 var h = window.innerHeight;
 if (w < 900) {
@@ -23,19 +28,60 @@ var mySound;
 var score;
 var coinSound;
 var deathSound;
+var powerUpSound;
 var dy = 0;
+var powerUpTimeOut = null;
 var food = {
   x: null,
   y: null,
   color: null,
 }
+var powerUp = {
+  x: null,
+  y: null,
+  exists: null,
+  id: null
+}
 var occupied = [];
+
+function generateRandomX() {
+  var result = Math.floor(20 * Math.random());
+  for (var i = 0; i < walls.length; i++) {
+    if (result == walls[i].x) {
+      result = generateRandomX();
+      break;
+    }
+  }
+  return result;
+}
+function generateRandomY() {
+  var result = Math.floor(20 * Math.random());
+  for (var i = 0; i < walls.length; i++) {
+    if (result == walls[i].y) {
+      result = generateRandomY();
+      break;
+    }
+  }
+  return result;
+}
+function clearPowerUp() {
+  powerUp.exists = null;
+  powerUp.x = null;
+  powerUp.y = null;
+}
+
 function createFood() {
-
-  food.x = Math.floor(20 * Math.random());
-  food.y = Math.floor(20 * Math.random());
-
+  food.x = generateRandomX();
+  food.y = generateRandomY();
   food.color = 'red';
+  if (Math.random() >= powerProb) {
+    clearPowerUp();
+    clearTimeout(powerUpTimeOut);
+    powerUp.exists = true;
+    powerUp.x = generateRandomX();
+    powerUp.y = generateRandomY();
+    powerUpTimeOut = setTimeout(clearPowerUp, 10000);
+  }
 }
 var tempdx, tempdy, temp1, temp2;
 class Block {
@@ -78,6 +124,7 @@ class Block {
 class Snake {
   constructor() {
     this.head = new Block(0, 0, 1, 0);
+    this.level = 1;
 
   }
   move() {
@@ -141,39 +188,122 @@ function checkFoodEat() {
     createFood();
     coinSound.play()
   }
+  if (snake.head.x == powerUp.x && snake.head.y == powerUp.y) {
+    snake.push();
+    setTimeout(snake.push(), 200);
+    setTimeout(snake.push(), 200);
+    setTimeout(snake.push(), 400);
+    setTimeout(snake.push(), 400);
+    powerUp.exists = null;
+    powerUpSound.play();
+
+  }
 }
 function lost() {
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+
   mySound.pause();
   deathSound.play();
   ctx.fillStyle = scoreColor;
   ctx.font = "50px Arial";
   clearInterval(interval);
+  interval = null;
+  clearTimeout(powerUpTimeOut);
   ctx.textAlign = "center";
-
-  ctx.fillText("Game Over!", myCanvas.width / 2, 200);
+  ctx.fillStyle = gridColor;
+  ctx.fillStyle = "black";
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'yellow';
+  ctx.textAlign = "center";
+  ctx.fillText("Game Over!", canvas.width / 2, 100);
   ctx.font = "20px Arial";
   ctx.fillStyle = foodColor;
-  ctx.fillText(`Score : ${score}`, myCanvas.width / 2, 250);
-  ctx.fillText(`Press Start Game to replay.`, myCanvas.width / 2, 280);
+  score = levelUp * (snake.level) + score;
+  ctx.fillText(`Score : ${score}`, canvas.width / 2, 150);
+  ctx.fillText(`Press Start Game to replay.`, canvas.width / 2, 180);
   ctx.fillStyle = 'white';
-  ctx.fillText(`Programmed by Divyanshu Agrawal.`, myCanvas.width / 2, 310);
+  ctx.fillText(`Programmed by Divyanshu Agrawal.`, canvas.width / 2, 210);
 }
+
+function createLevel(level) {
+
+  snake = null;
+  score = 0;
+  snake = new Snake();
+  snake.level = level;
+  occupied = [];
+  clearInterval(interval);
+  if (level == 2) {
+    window.interval = setInterval(drawGame, 100);
+    snakeColor = "rgb(145, 231, 136)";
+    walls = [];
+    walls.push({ x: 0, y: 9 });
+    walls.push({ x: 1, y: 9 });
+    walls.push({ x: 2, y: 9 });
+    walls.push({ x: 3, y: 9 });
+    walls.push({ x: 4, y: 9 });
+    walls.push({ x: 5, y: 9 });
+    walls.push({ x: 19, y: 5 });
+    walls.push({ x: 18, y: 5 });
+    walls.push({ x: 17, y: 5 });
+    walls.push({ x: 16, y: 5 });
+    walls.push({ x: 15, y: 5 });
+  }
+}
+
+function checkLevel(level) {
+  // console.log(level);
+}
+
 function drawGrid() {
+
   ctx.fillStyle = gridColor;
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (powerUp.exists) {
+    // console.log("Rendered");
+    ctx.fillStyle = powerUpColor;
+    // console.log("powerup:", powerUp.x, powerUp.y, scale, scale);
+    ctx.fillRect(powerUp.x * scale, powerUp.y * scale, scale, scale);
+  }
   var block = snake.head;
   ctx.fillStyle = snakeColor;
   checkFoodEat();
   score = occupied.length * 100 - 200;
 
+  // Increase Levels
+
+  if (walls.length > 0) {
+    ctx.fillStyle = wallColor;
+    for (var i = 0; i < walls.length; i++) {
+      ctx.fillRect(walls[i].x * scale, walls[i].y * scale, scale, scale);
+    }
+  }
+
+  if (score > levelUp && snake.level != 2) {
+    createLevel(2);
+    checkLevel(2);
+  }
+
   ctx.fillStyle = snakeColor;
+
+
+  // Check for Dead Snake 
   for (var i = 1; i < occupied.length; ++i) {
     if (snake.head.x == occupied[i].x && snake.head.y == occupied[i].y) {
       lost()
-    } else {
+      return;
     }
   }
+
+  for (var i = 0; i < walls.length; i++) {
+    if (snake.head.x == walls[i].x && snake.head.y == walls[i].y) {
+      lost()
+      return;
+    }
+  }
+
+
+  // Render Snake Body
+
   while (block != null) {
 
     ctx.fillRect(block.x * scale, block.y * scale, scale, scale);
@@ -181,9 +311,24 @@ function drawGrid() {
   }
   ctx.fillStyle = food.color;
   ctx.fillRect(food.x * scale, food.y * scale, scale, scale);
+
+  if (snake.level > 1) {
+    console.log(snake.level);
+    score += levelUp * (snake.level - 1) + 200;
+  } else {
+  }
   ctx.fillStyle = scoreColor;
-  ctx.font = "20px Arial";
-  ctx.fillText(score, 20, 20);
+  if (w < 900) {
+    ctx.font = '10px Arial';
+    ctx.fillText(`Score: ${score}`, 5, 10);
+    ctx.fillText(`Level : ${snake.level}`, scale * 18, 10);
+  } else {
+    ctx.font = "20px Arial";
+    ctx.fillText(`Score: ${score}`, 25, 20);
+    ctx.fillText(`Level : ${snake.level}`, 25, 45);
+
+
+  }
 
 }
 
@@ -199,16 +344,16 @@ function drawGame() {
 // startGame()
 
 function startGame() {
-  if (mySound) {
-    mySound.pause();
+  if (snake) {
+    window.location.reload();
   }
-  clearInterval(interval);
   snake = new Snake();
   score = 0;
   occupied = [];
   snake.push();
   mySound = new Audio('sound.mp3');
   mySound.pause();
+  powerUpSound = new Audio('powerup.wav');
   coinSound = new Audio('food.wav');
   deathSound = new Audio('death.wav');
   mySound.loop = true;
